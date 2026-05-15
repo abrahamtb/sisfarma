8743fe4916b0a14712534c5c5ae80e2625f7f082
Abraham <abrahamtkm123@gmail.com>
2026-05-14 19:18:00 -0500

    Primer commit SisFarma

---
+++ js/script.js
@@ -0,0 +1,23 @@
+AOS.init({
+    duration: 1000,
+    once: true
+});
+
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('keyup', () => {
+
+    let value = searchInput.value.toLowerCase();
+
+    if(value.includes('paracetamol')) {
+        alert('✅ Paracetamol disponible en SisFarma');
+    }
+
+    if(value.includes('ibuprofeno')) {
+        alert('✅ Ibuprofeno disponible en SisFarma');
+    }
+
+    if(value.includes('vitamina')) {
+        alert('✅ Vitaminas disponibles');
+    }
+});
